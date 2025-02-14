19e20504df78fe2c3c11d286c40e5027a6357708
seyun0309 <157377184+seyun0309@users.noreply.github.com>
2025-02-14 23:34:54 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 1.24 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        
+        return (int) Arrays.stream(array)
+            .filter(n -> n > height)
+            .count();
+    }
+}
