3b67d8e54fc66a5b67ad737266ea6efe49b2ffa8
seyun0309 <157377184+seyun0309@users.noreply.github.com>
2025-02-16 21:41:09 +0900

    [level 0] Title: 배열의 평균값, Time: 3.92 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        
+        return Arrays.stream(numbers)
+            .average()
+            .orElse(0);
+    }
+}
