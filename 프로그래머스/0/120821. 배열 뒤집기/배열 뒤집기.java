6bb3e592bf32b907292ca466c39f1388539dfb4f
seyun0309 <157377184+seyun0309@users.noreply.github.com>
2025-02-16 22:14:40 +0900

    [level 0] Title: 배열 뒤집기, Time: 2.99 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        
+        return IntStream.range(0, num_list.length)
+                .map(i -> num_list[num_list.length - 1 - i])
+                .toArray();
+    }
+}
