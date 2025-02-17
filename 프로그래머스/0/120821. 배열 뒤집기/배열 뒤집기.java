fe20f65b98677cd18566e8899db4be92db9bbdb8
seyun0309 <157377184+seyun0309@users.noreply.github.com>
2025-02-17 22:13:01 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -2,10 +2,15 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = {};
+        int[] answer = new int[num_list.length];
         
-        return IntStream.range(0, num_list.length)
-                .map(i -> num_list[num_list.length - 1 - i])
-                .toArray();
+        for(int i=num_list.length-1; i>=0; i--) {
+            answer[answer.length-i-1] = num_list[i];
+        }
+        
+        return answer;
+        // return IntStream.range(0, num_list.length)
+        //         .map(i -> num_list[num_list.length - 1 - i])
+        //         .toArray();
     }
 }
